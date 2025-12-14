8bbd091204b71db6a7ce64239d641ff886f7d392
meowsus <tooblies@gmail.com>
2025-12-14 09:28:27 -0800

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,48 @@
 <div class="flex flex-col gap-6 px-6 py-2">
 	<section class="flex flex-col gap-4 text-lg">
-		<p>
-			It's 2025 and you're staring at yet another iteration of fauxfetus. I'm sorry to keep doing
-			this to you. Not much has changed since the last time I decided to resurrect this corpse. The
-			main reason I felt compelled to do it all over again was because I almost immediately lost the
-			source code for the current site. That means that I'd have a hell of a time trying to get it
-			back up and running, if anything were to happen. I had a weekend at my disposal and I wanted
-			to teach myself yet another web framework, so here we are and for better or for worse.
-		</p>
+		<ul class="timeline timeline-vertical timeline-compact">
+			<li>
+				<div class="timeline-start">v1.1.0</div>
+				<div class="timeline-middle">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</div>
+				<div class="timeline-end timeline-box">
+					Dec 14, 2025 - I added basic SEO. Pretty boring, tbh. It'll get good soon.
+				</div>
+				<hr />
+			</li>
+			<li>
+				<div class="timeline-start">v1.0.0</div>
+				<div class="timeline-middle">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</div>
+				<div class="timeline-end timeline-box">
+					Nov 30, 2025 - I've relaunched the site. Don't ask me why.
+				</div>
+				<hr />
+			</li>
+		</ul>
 	</section>
 </div>
