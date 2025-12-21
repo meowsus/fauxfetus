c8feafa31365a71f0882332922de2db025f611ae
meowsus <tooblies@gmail.com>
2025-12-20 17:01:49 -0800

    Componentize timeline

---
+++ src/lib/components/Timeline.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let entries: Array<{
+		date: string;
+		description: string;
+	}> = [];
+</script>
+
+<ul class="timeline timeline-vertical timeline-compact">
+	{#each entries as entry (entry.date)}
+		<li>
+			<div class="timeline-start">{entry.date}</div>
+			<div class="timeline-middle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					class="h-5 w-5"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div class="timeline-end timeline-box">
+				{entry.description}
+			</div>
+			<hr />
+		</li>
+	{/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,20 @@
+<script lang="ts">
+	import Timeline from '$lib/components/Timeline.svelte';
+
+	const timelineEntries = [
+		{
+			date: 'Dec 14, 2025',
+			description: "I added basic SEO. Pretty boring, tbh. It'll get good soon."
+		},
+		{
+			date: 'Nov 30, 2025',
+			description: "I've relaunched the site. Don't ask me why."
+		}
+	];
+</script>
+
 <div class="flex flex-col gap-6 px-6 py-2">
 	<section class="flex flex-col gap-4 text-lg">
-		<ul class="timeline timeline-vertical timeline-compact">
-			<li>
-				<div class="timeline-start">v1.1.0</div>
-				<div class="timeline-middle">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-						class="h-5 w-5"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				</div>
-				<div class="timeline-end timeline-box">
-					Dec 14, 2025 - I added basic SEO. Pretty boring, tbh. It'll get good soon.
-				</div>
-				<hr />
-			</li>
-			<li>
-				<div class="timeline-start">v1.0.0</div>
-				<div class="timeline-middle">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-						class="h-5 w-5"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				</div>
-				<div class="timeline-end timeline-box">
-					Nov 30, 2025 - I've relaunched the site. Don't ask me why.
-				</div>
-				<hr />
-			</li>
-		</ul>
+		<Timeline entries={timelineEntries} />
 	</section>
 </div>
