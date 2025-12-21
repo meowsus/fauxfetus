b9f483f8d0b4d30e98b2edfd41dcc93fca42f004
meowsus <tooblies@gmail.com>
2025-12-21 08:35:20 -0800

    Announce

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,53 @@
 <div class="flex flex-col gap-6 px-6 py-2">
 	<section class="flex flex-col gap-4 text-lg">
 		<Timeline>
+			<TimelineEntry date="Dec 21, 2025">
+				<p class="mb-2">
+					Okay, this is actually exciting: I added six split releases that have been sitting around
+					for far too long. This means that we welcome a few "new" "artists" into the fold, namely
+					J. Mono & Miles Rozatti. Welcome. I'm sure you're SO happy to be here. We also welcome
+					back Happybear Kaboom, which will likely be taken down soon, so listen while you can.
+					Here's the list:
+				</p>
+
+				<ul class="list-inside list-disc">
+					<li>
+						<a href={resolve('/artists/bornpilot/bornpilot-welcome-wizard')} class="link-primary"
+							>Bornpilot / Welcome Wizard</a
+						>
+					</li>
+					<li>
+						<a
+							href={resolve('/artists/captain-werewolf/captain-werewolf-miles-rozatti')}
+							class="link-primary">Captain Werewolf / Miles Rozatti</a
+						>
+					</li>
+					<li>
+						<a
+							href={resolve('/artists/father-sleep/happybear-kaboom-father-sleep')}
+							class="link-primary">Happybear Kaboom / Father Sleep</a
+						>
+					</li>
+					<li>
+						<a
+							href={resolve('/artists/the-cold-lamper/the-cold-lamper-j-mono')}
+							class="link-primary">The Cold Lamper / J. Mono</a
+						>
+					</li>
+					<li>
+						<a
+							href={resolve('/artists/the-night-owl/the-night-owl-welcome-wizard')}
+							class="link-primary">The Night Owl / Welcome Wizard</a
+						>
+					</li>
+					<li>
+						<a
+							href={resolve('/artists/the-riffingtons/the-riffingtons-eat-taco-enema')}
+							class="link-primary">The Riffingtons / EAT</a
+						>
+					</li>
+				</ul>
+			</TimelineEntry>
 			<TimelineEntry date="Dec 20, 2025">
 				I added <a href={resolve('/artists/aunt-vivians-revengence')} class="link-primary"
 					>Aunt Vivian's Revengence</a
