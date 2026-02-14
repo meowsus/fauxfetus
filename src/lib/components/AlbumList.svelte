76396860444495349621612c34525271d7c5c3c0
meowsus <tooblies@gmail.com>
2026-02-14 14:34:58 -0800

    Add compilation flag to albums

--- src/lib/components/AlbumList.svelte
+++ src/lib/components/AlbumList.svelte
@@ -10,11 +10,15 @@
 
 	{#each albums as album (album.slug)}
 		<li class="list-row">
-			<div>
+			<div class="flex items-center gap-2">
 				<a
 					href={resolve(`/artists/${album.artistSlug}/${album.slug}`)}
 					class="hover:link-primary/80 link text-lg link-primary">{album.name}</a
 				>
+
+				{#if album.isCompilation}
+					<span class="badge badge-outline badge-sm badge-info">Compilation</span>
+				{/if}
 			</div>
 		</li>
 	{/each}
