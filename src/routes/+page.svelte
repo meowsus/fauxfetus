1f144cd1aae53ef99169f12df906aff2929fb05c
meowsus <tooblies@gmail.com>
2026-02-15 18:26:56 -0800

    whoops

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { marked } from 'marked';
 	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
@@ -8,7 +7,7 @@
 <article
 	class="prose prose-base max-w-none py-8 md:prose-lg dark:prose-invert prose-headings:text-base-content/80 prose-a:text-primary prose-a:underline prose-a:decoration-primary prose-a:underline-offset-2 prose-a:hover:opacity-80"
 >
-	{#await marked.parse(data.markdown) then html}
-		{@html html}
-	{/await}
+	<!-- Server-rendered HTML from trusted markdown -->
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+	{@html data.html}
 </article>
